fb7322e6cccb4db080cbe7f2f2cb55235f19fc8f
Sacha Isambourg <sacha.isambourg01@gmail.com>
2024-03-19 14:06:27 +0100

    endJS

---
+++ index.js
@@ -0,0 +1,15 @@
+// export const test = "lalalala"
+
+
+
+ const inputPseudo = localStorage.document.querySelector(".Pseudonyme");
+
+
+const run = document.getElementsByClassName("run")
+
+let pseudoValue = "";
+run.addEventListener("click", function() {}
+    pseudoValue = inputPseudo.target.value;
+    console.log(pseudoValue);
+
+// const inputRun = document.getElementByClassName("run");
